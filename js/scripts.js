59143340501bf9d44d613b5348af697e31bf8a80
Kipkorir-Gideon <gideon.ngetich@student.moringaschool.com>
2021-09-18 15:15:53 +0300

    Add variables to store input from the user on size, crust and toppings.

--- js/scripts.js
+++ js/scripts.js
@@ -7,3 +7,17 @@ function Order(size, crust, toppings) {
 Order.prototype.orderCost = function() {
     return this.size + this.crust + this.toppings;
 }
+
+
+
+
+
+$(document).ready(function() {
+    $("form#new-order").submit(function(event) {
+        event.preventDefault();
+
+        var inputtedSize = $("select#selection1 option:selected").val();
+        var inputtedCrust = $("select#selection2 option:selected").val();
+        var inputtedTopping = $("#selection3 input:checked").val();
+    })
+})
